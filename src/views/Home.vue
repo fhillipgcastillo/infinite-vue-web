227e56f6d4601008eaed33a99882f1fa7b695db2
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2020-02-19 00:21:10 -0400

    Added basic router and vuex

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ToDo />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import ToDo from '@/components/ToDo.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ToDo
+  }
+}
+</script>
